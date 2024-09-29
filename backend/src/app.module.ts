877d0a247d5d066d24789684bda10c865d4363c3
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-09-30 00:42:29 +0300

    chore: fix username, add TypeOrm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'bekinduser',
+      password: 'bekindpassword',
+      database: 'bekinddb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
