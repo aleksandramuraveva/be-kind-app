2ecdec07161908e0225c681b141da492a7594738
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-09-30 03:50:00 +0300

    chore: fix database connection issues and update docker-compose configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,16 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'db',
-      port: 5432,
-      username: 'bekinduser',
-      password: 'bekindpassword',
-      database: 'bekinddb',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
@@ -20,4 +23,6 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule {}
+
+
 
