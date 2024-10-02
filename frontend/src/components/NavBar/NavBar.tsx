e280dd702c1d27edf6f8eb7d70c7707744f8d78e
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-10-03 00:38:10 +0300

    refactor: change corners on navlinks

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -8,21 +8,21 @@ const NavBar = () => {
           <Link href="/" className="text-white px-6 py-1 block relative z-10">
             Home
           </Link>
-          <span className="absolute left-0 top-0 w-full h-full bg-blue-500 transform group-hover:bg-blue-600 transition duration-300"></span>
+          <span className="rounded absolute left-0 top-0 w-full h-full bg-blue-500 transform group-hover:bg-blue-600 transition duration-300"></span>
         </li>
 
         <li className="relative group ribbon transform transition duration-300">
           <Link href="/friends" className="text-white px-6 py-1 block relative z-10">
             Friends
           </Link>
-          <span className="absolute left-0 top-0 w-full h-full bg-blue-500 transform group-hover:bg-blue-600 transition duration-300"></span>
+          <span className="rounded absolute left-0 top-0 w-full h-full bg-blue-500 transform group-hover:bg-blue-600 transition duration-300"></span>
         </li>
 
         <li className="relative group ribbon transform transition duration-300">
           <Link href="/profile" className="text-white px-6 py-1 block relative z-10">
             Profile
           </Link>
-          <span className="absolute left-0 top-0 w-full h-full bg-blue-500 transform group-hover:bg-blue-600 transition duration-300"></span>
+          <span className="rounded absolute left-0 top-0 w-full h-full bg-blue-500 transform group-hover:bg-blue-600 transition duration-300"></span>
         </li>
 
         <li className="absolute right-0">
