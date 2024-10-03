0d3fb9d7384d5392707bdff01205118de5b47407
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-10-03 10:31:23 +0300

    feature: style dashboard component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,30 +1,32 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import Dashboard from "../components/Dashboard/Dashboard";
+import HomeText from "../components/HomeText/HomeText";
 
 export default function Home() {
-  const [message, setMessage] = useState("");
+  // const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    console.log('Fetching data from:', `${process.env.NEXT_PUBLIC_API_URL}/test`);
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/test`)
-      .then(res => res.text())
-      .then(data => {
-        console.log('Received data:', data);
-        setMessage(data);
-      })
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
+  // useEffect(() => {
+  //   console.log('Fetching data from:', `${process.env.NEXT_PUBLIC_API_URL}/test`);
+  //   fetch(`${process.env.NEXT_PUBLIC_API_URL}/test`)
+  //     .then(res => res.text())
+  //     .then(data => {
+  //       console.log('Received data:', data);
+  //       setMessage(data);
+  //     })
+  //     .catch(error => console.error('Error fetching data:', error));
+  // }, []);
 
   return (
   
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-open-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <div className="">{message} hello</div>
-          <Dashboard/>
-        </main>
-      </div>
+   <div className="min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-open-sans)]">
+      <main className="flex flex-col gap-8 items-center sm:items-start max-w-screen-lg mx-auto">
+       {/* <div>{message} hello</div>*/}
+        <HomeText />
+        <Dashboard />
+      </main>
+    </div>
 
   );
 }
--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -1,11 +1,20 @@
+import React from 'react';
 import GoodDeedsList from "../GoodDeedsList/GoodDeedsList";
 
-const Dashboard = () => {
+const Dashboard = ({ friendName }) => {
+  const displayName = friendName || "Your";
+  const title = displayName === "Your" ? "Your Good Deeds" : `${displayName}'s Good Deeds`;
+
   return (
-    <div>
-      <p>Dashboard Content</p>
-      <GoodDeedsList/>
-    </div>
+    <section className="relative  p-4 mt-10">
+      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <img src="/images/wings.png" alt="Wings" className="filter brightness-110" />
+      </div>
+      <div className="text-center mb-4 mt-6">
+        <h2 className="text-shadow3 text-my-blue text-xl font-bold">{title}</h2>
+      </div>
+      <GoodDeedsList />
+    </section>
   );
 };
 
