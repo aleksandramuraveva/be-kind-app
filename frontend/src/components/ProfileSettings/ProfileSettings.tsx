781cfb9f54ffd723338a862b8d8d78b042b84005
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-10-10 19:04:41 +0300

    refactor: display name and tag in the settings

--- frontend/src/components/ProfileSettings/ProfileSettings.tsx
+++ frontend/src/components/ProfileSettings/ProfileSettings.tsx
@@ -1,21 +1,29 @@
+'use client'
 
-
+import React from 'react';
 import UsernameForm from './UsernameForm/UsernameForm';
 import EmailForm from './EmailForm/EmailForm';
 import PasswordForm from './PasswordForm/PasswordForm';
 import DeleteAccountButton from './DeleteAccountButton/DeleteAccountButton';
 
 const ProfileSettings = () => {
+  const username = (typeof window !== 'undefined') ? localStorage.getItem('username') : '';
+  const uniqueId = (typeof window !== 'undefined') ? localStorage.getItem('uniqueId') : '';
+
   return (
     <div className="p-8 space-y-8 mx-auto mb-20 text-white">
+      <div className=" mb-4 font-bold">
+        <p className="text-2xl">Name: {username}</p>
+        <p className="text-2xl">Tag: {uniqueId}</p>
+      </div>
       <h1 className="text-2xl text-white font-bold mb-4">Profile Settings</h1>
       <UsernameForm />
       <EmailForm />
       <PasswordForm />
       <DeleteAccountButton />
-
     </div>
   );
 };
 
 export default ProfileSettings;
+
