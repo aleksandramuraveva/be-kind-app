b7b150a4fad96ae10b8ca51e3ede502f298b348a
aleksandramuraveva <84822235+aleksandramuraveva@users.noreply.github.com>
2024-10-16 16:14:37 +0300

    fix: cors issues

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: 'https://be-kind-app-frontend-latest.onrender.com', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   const port = process.env.PORT || 4000;
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
+
